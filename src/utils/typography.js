f3fef5e69116bc523945242b8036a4e5e9690ffa
THATBLADEBOY <aus10b@gmail.com>
2020-02-13 07:30:16 -0600

    saved typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,13 @@
 import Typography from "typography"
 import fairyGateTheme from "typography-theme-fairy-gates"
 
+fairyGateTheme.overrideThemeStyles: ({ rhythm }, options, styles) => ({
+    'h2,h3': {
+        marginBottom: rhythm(1/2),
+        marginTop: rhythm(2),
+    }
+})
+
 const typography = new Typography(fairyGateTheme)
 
 export const { scale, rhythm, options } = typography
